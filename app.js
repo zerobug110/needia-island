1e280673d149832afb9f377dde2fc738bbdc80ff
zerobug110 <mahabojang110@gmail.com>
2020-09-07 00:04:21 +0000

    complete the animation on hamburger menus

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //variables
 const burger = document.querySelector('.lines');
 const navLinks = document.querySelector('.nav-bar__items')
+const open = document.querySelector('.nav-bar__show');
 const links = document.querySelectorAll('.nav-bar__links')
 
 burger.addEventListener('click', () => {
-    navLinks.classList.toggle('.show');
+    navLinks.classList.toggle('nav-bar__show');
 });
